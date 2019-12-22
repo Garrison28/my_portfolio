50c928deaa2bb60f5d2d66ff24daaeb985e8b7a0
Garrison28 <ghighsmith28@gmail.com>
2019-12-22 09:18:11 -0800

    added body and footer

--- src/App.jsx
+++ src/App.jsx
@@ -19,36 +19,63 @@ import styles from "/assets/jss/material-kit-react/components/headerStyle.js";
 const useStyles = makeStyles(styles);
 
 function Profile(props) {
-  render {
-    return (
-      <>
-        <div className="bkgrd-img">
-          <header>
-            <div className="nav-bar ">
-              <nav>
-                <Link to="" >About</Link>{' | '}
-                <Link>Projects</Link>{' | '}
-                <Link>Contact</Link>{' | '}
-              </nav>
+  return (
+    <>
+      <div className="bkgrd-img">
+        <header>
+          <div className="nav-bar ">
+            <nav>
+              <Link to="" >About</Link>{' | '}
+              <Link>Projects</Link>{' | '}
+              <Link>Contact</Link>{' | '}
+            </nav>
+          </div>
+        </header>
+        <body>
+          <div className="contents">
+            <div className="about">
+              <h1>About</h1>
+              <p>My name is Garrison, a musician and service industry worker turned full stack engineer</p>
+
             </div>
-          </header>
-          <body>
-            <div className="contents">
-              <div className="about">
-                <h1>About</h1>
-                <p>My name is Garrison, a musician and service industry worker turned full stack engineer</p>
+            <div className="project">
+              <h2>Projects</h2>
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+              <div>
 
               </div>
             </div>
-          </body>
-        </div>
-      </>
-    )
-  }
+            <div>
+              <h2>Contact</h2>
+              <span>Email:</span>
+              <div>
 
+              </div>
+              <div>
 
-
+              </div>
+            </div>
+          </div>
+        </body>
+        <footer>
+          
+        </footer>
+      </div>
+    </>
+  )
 }
 
 export default Profile;
+
+
+
+
 
