a0f497a9dbc92e82d7704693de4ab89dcc558b2f
Garrison28 <ghighsmith28@gmail.com>
2019-12-31 12:13:10 -0800

    added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,183 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
-// import PropTypes from "prop-types";
-// import classNames from "classnames";
-// import { makeStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import IconButton from "@material-ui/core/IconButton";
-// import Button from "@material-ui/core/Button";
-// import Hidden from "@material-ui/core/Hidden";
+import Carousel from './carousel/Carousel';
+import { makeStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
-// import Drawer from "@material-ui/core/Drawer";
+const useStyles = makeStyles({
+  home: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  prompt: {
+    color: 'black',
+  },
+  button: {
+    width: '50vw',
+    height: '10vh',
+    backgroundColor: '#2E88D1',
+  }
 
-// import Menu from "@material-ui/icons/Menu";
-
-// import styles from "/assets/jss/material-kit-react/components/headerStyle.js";
-
-// const useStyles = makeStyles(styles);
+})
 
 function Profile(props) {
+  const classes = useStyles()
+  const projectOne = [
+
+    {
+      name: 'Gold',
+      image: '/img/Gold.png'
+    },
+    {
+      name: 'Level Cleared',
+      image: '/img/GoldLevelClear.png'
+    }
+  ]
+
+  const projectTwo = [
+    {
+      name: 'ArtistGO!',
+      image: '/img/ArtistGoHome.png'
+    },
+    {
+      name: 'Home Page',
+      image: '/img/ArtistGoMainPage.png'
+    },
+    {
+      name: 'Search Page',
+      image: '/img/ArtistGoSearch.png'
+    },
+    {
+      name: 'Favorite Artist Page',
+      image: '/img/ArtistGoMyArtist.png'
+    }
+  ]
+
+  const projectThree = [
+    {
+      name: 'Stuter',
+      image: '/img/StuterLoginPage.png'
+    },
+    {
+      name: 'Stuter Dashboard',
+      image: '/img/StuterDashboard.png'
+    },
+    {
+      name: 'Stuter Subjects',
+      image: '/img/StuterSubjects.png'
+    },
+    {
+      name: 'Stuter Tutors',
+      image: '/img/StuterTutor.png'
+    }
+  ]
+
+  const projectFour = [
+    {
+      name: 'The Photo',
+      image: '/img/ThePhotoHome.png'
+    },
+    {
+      name: 'The Photo Categories',
+      image: '/img/ThePhotoCategories.png'
+    },
+    {
+      name: 'The Photo Photos',
+      image: '/img/ThePhotoPhotos.png'
+    }
+  ]
+
   return (
     <>
-      <div className="bkgrd-img">
+      <div className="container bkgrd-img">
         <header>
-          <div className="nav-bar ">
+          <div className="nav-bar card">
             <nav>
-              <a href="" >About</a>{' | '}
-              <a>Projects</a>{' | '}
-              <a>Contact</a>{' | '}
+              <a href="#about-me" >About</a>{' | '}
+              <a href="#my-projects">Projects</a>{' | '}
+              <a href="#contact-me">Contact</a>{' | '}
             </nav>
           </div>
         </header>
         <body>
           <div className="contents">
-            <div className="about">
+            <div id="about-me" className="about">
               <h1>About</h1>
-              <p>My name is Garrison, a musician and service industry worker turned full stack engineer</p>
+              <div className="card-one">
+                <p>My name is Garrison, A jazz musician turned full stack web developer. I enjoy finding creative solutions that require out of the box thinking for out of the box problems. Being a jazz musician allows you to think in a more lateral way, especially during improvisation; this is a transferable skill that would allow to look at certain problems in development.</p>
+              </div>
 
             </div>
-            <div className="project">
-              <h2>Projects</h2>
-              <div>
-                <h3>Project 1</h3>
-                <a href="https://garrison28.github.io/Gold/">Gold!</a>
-
+            <div id="my-projects" className="project">
+              <h1 className="">Projects</h1>
+              <div className="">
+                <h3 className="card">Project 1</h3>
+                <div className={classes.home}>
+                  <Carousel items={projectOne}
+                  /><br />
+                  <p>Get all the gold pieces before the enemies Get to you.</p>
+                  <a href="https://garrison28.github.io/Gold/" target="_blank">
+                    <Button className={classes.button}>Play the Game!</Button>
+                  </a><br/>
+                  <a href="https://github.com/Garrison28/Gold" target="_blank">
+                    <Button className={classes.button}>Github Repo</Button>
+                  </a>
+                </div>
               </div>
-              <div>
-                <h3>Project 2</h3>
-                <a href="https://morning-cliffs-87357.herokuapp.com/">Artist GO</a>
 
+              <div className="">
+                <h3 className="card">Project 2</h3>
+                <div className={classes.home}>
+                  <Carousel items={projectTwo}
+                  /><br />
+                  <p></p>
+                  <a href="https://morning-cliffs-87357.herokuapp.com/" target="_blank">
+                    <Button className={classes.button}>Go to ArtistGO!</Button>
+                  </a><br/>
+                  <a href="https://github.com/Garrison28/my-Travel-Fest" target="_blank">
+                    <Button className={classes.button}>Github Repo</Button>
+                  </a>
+                </div>
+                
               </div>
-              <div>
+              <div className="">
                 <h3>Project 3</h3>
-                <a href="https://polar-sands-42976.herokuapp.com/">Stuter</a>
+                <div className={classes.home}>
+                  <Carousel items={projectThree}
+                  /><br />
+                  <a href="https://polar-sands-42976.herokuapp.com/" target="_blank">
+                    <Button className={classes.button}>Go Stuter!</Button>
+                  </a><br/>
+                  <a href="https://github.com/Garrison28/stuter" target="_blank">
+                    <Button className={classes.button}>Github Repo</Button>
+                  </a>
+                </div>
 
               </div>
-              <div>
+              <div className="">
                 <h3>Project 4</h3>
-                <a href='https://secret-ridge-94962.herokuapp.com'>The Photo</a>
+                <div className={classes.home}>
+                  <Carousel items={projectFour}
+                  /><br />
+                  <a href='https://secret-ridge-94962.herokuapp.com' target="_blank">
+                    <Button className={classes.button}>Go The Photo!</Button>
+                  </a><br/>
+                  <a href="https://github.com/Garrison28/The-Photo" target="_blank">
+                    <Button className={classes.button}>Github Repo</Button>
+                  </a>
+                </div>
 
               </div>
             </div>
-            <div>
+            <div id="contact-me" className="card">
               <h2>Contact</h2>
               <span>Email: ghighsmith28@gmail.com</span>{' | '}
-              <span>Linkdin: <a href='https://www.linkedin.com/in/garrison-highsmith'> My Linkdin</a></span>{' | '}
-              <span>Github: <a href='https://github.com/Garrison28'> My Github</a></span>
+              <span>Linkdin: <a href='https://www.linkedin.com/in/garrison-highsmith' target="_blank"> My Linkdin</a></span>{' | '}
+              <span>Github: <a href='https://github.com/Garrison28' target="_blank"> My Github<i></i></a></span>
               <div>
 
               </div>
@@ -82,6 +194,7 @@ function Profile(props) {
     </>
   )
 }
+
 
 export default Profile;
 
---
+++ src/carousel/Carousel.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    carousel: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    carouselImage: {
+        height: '50vh',
+        width: '50vw',
+        borderStyle: 'solid',
+        borderColor: '#2E88D1',
+    },
+    name: {
+        color: 'black',
+    },
+    prevItem:{
+        color: 'black',
+    },
+    nextItem:{
+        color: 'black',
+    },
+    carouselContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
+})
+
+const Carousel = (props) => {
+    const classes = useStyles()
+    const [carouselIndex, setCarouselIndex] = useState(0);
+    const [current, setCurrent] = useState({});
+
+    useEffect(() => {
+        setCurrent(props.items[carouselIndex])
+    })
+
+    const nextItem = () => {
+        if (carouselIndex >= props.items.length - 1) {
+            setCarouselIndex(0)
+        }
+        else {
+            setCarouselIndex(carouselIndex + 1)
+        }
+        setCurrent(props.items[carouselIndex])
+    }
+
+    const prevItem = () => {
+        if ( carouselIndex > 0) {
+            setCarouselIndex(carouselIndex - 1)
+        }
+        else {
+            setCarouselIndex(props.items.length - 1)
+        }
+        setCurrent(props.items[carouselIndex])
+    }
+
+    const handleClick = () => {
+        props.updateData(current.name)
+        props.updateImage(current.image)
+    }
+
+    return (
+        <div className={classes.carousel}>
+            <ChevronLeftIcon className={classes.prevItem} onClick={prevItem} />
+            <div className={classes.carouselContent}>
+                <h4 className={classes.name}>{current.name}</h4>
+                <img className={classes.carouselImage} src={current.image} />
+            </div>
+            <ChevronRightIcon className={classes.nextItem} onClick={nextItem} />
+        </div>
+    )
+}
+
+export default Carousel;
