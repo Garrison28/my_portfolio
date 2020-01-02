7e8ddb161fe0f6051f5734edf0fc22052a7854c1
Garrison28 <ghighsmith28@gmail.com>
2020-01-02 14:38:29 -0800

    Updates

---
+++ precache-manifest.00deed5c61b6d36e36405e9de5525ef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143626541b0500510ad0102eece9e9a4",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "8e2081ba163785033169",
+    "url": "/my_portfolio/static/css/main.74b10c11.chunk.css"
+  },
+  {
+    "revision": "7f9dc2569237346a8096",
+    "url": "/my_portfolio/static/js/2.352500ca.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my_portfolio/static/js/2.352500ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e2081ba163785033169",
+    "url": "/my_portfolio/static/js/main.edb4de65.chunk.js"
+  },
+  {
+    "revision": "75f4eaedd3ca3922ca28",
+    "url": "/my_portfolio/static/js/runtime-main.35f41595.js"
+  }
+]);
