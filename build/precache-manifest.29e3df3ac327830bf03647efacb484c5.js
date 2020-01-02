c81a5951f280b29d3edf2043ee07fae247cf4fe8
Garrison28 <ghighsmith28@gmail.com>
2020-01-02 14:12:16 -0800

    removed  .gitignore items

---
+++ build/precache-manifest.29e3df3ac327830bf03647efacb484c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0640bc720701cdc2297799064ab98c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdb9bc38ad88487d27a6",
+    "url": "/static/css/main.74b10c11.chunk.css"
+  },
+  {
+    "revision": "7f9dc2569237346a8096",
+    "url": "/static/js/2.352500ca.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.352500ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdb9bc38ad88487d27a6",
+    "url": "/static/js/main.acd6646c.chunk.js"
+  },
+  {
+    "revision": "b6d641d7babc028ccdf2",
+    "url": "/static/js/runtime-main.712fe5d1.js"
+  }
+]);
